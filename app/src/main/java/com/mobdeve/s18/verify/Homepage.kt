f71ac37216f84179f08293541e3a8ae99a8a3784
Julianne Mari Du Wong <136436248+julzmari@users.noreply.github.com>
2025-06-15 01:19:00 +0800

    register

--- app/src/main/java/com/mobdeve/s18/verify/Homepage.kt
+++ app/src/main/java/com/mobdeve/s18/verify/Homepage.kt
@@ -1,11 +1,20 @@
 package com.mobdeve.s18.verify
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class Homepage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_homepage)
+
+        val registerButton = findViewById<Button>(R.id.homepage_register_btn)
+
+        registerButton.setOnClickListener {
+            val intent = Intent(this, Register::class.java)
+            startActivity(intent)
+        }
     }
 }
